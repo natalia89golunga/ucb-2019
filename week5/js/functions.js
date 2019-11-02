0db55f429a09ee551c72d3a2abdfaf20bc33fbc7
Natalia <natalia89golunga@gmail.com>
2019-11-01 22:14:48 -0700

    fixed typo

--- week5/js/functions.js
+++ week5/js/functions.js
@@ -9,7 +9,7 @@ $("h2").click(function() {
 $(this).removeClass("highlight")
 })
 $("h1").click(function() {
-$(this).taggleClass("highlight")
+$(this).toggleClass("highlight")
   //$(this).addClass("highlight")
 })
 $("#al-dismiss").click(function(){
